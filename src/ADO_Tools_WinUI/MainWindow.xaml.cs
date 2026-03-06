4c9fb19f80dd44be775bfb42672afd3812a26407
Kivanc Karakas <kivanckarakas@gmail.com>
2026-03-06 21:18:37 +0000

    UI updates, title space claimed

--- src/ADO_Tools_WinUI/MainWindow.xaml.cs
+++ src/ADO_Tools_WinUI/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using ADO_Tools_WinUI.Pages;
 using ADO_Tools_WinUI.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using WinRT.Interop;
 
 namespace ADO_Tools_WinUI
 {
@@ -12,11 +13,26 @@ namespace ADO_Tools_WinUI
         {
             InitializeComponent();
 
-            // Set a reasonable desktop window size
-            var appWindow = this.AppWindow;
-            appWindow.Resize(new Windows.Graphics.SizeInt32(1100, 700));
+            // Extend content into the title bar area to reclaim vertical space
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+
+            // Resize using DPI-aware logical size so it looks correct at any scaling
+            ResizeWindowToLogicalSize(1280, 840);
 
             Closed += MainWindow_Closed;
+        }
+
+        private void ResizeWindowToLogicalSize(int logicalWidth, int logicalHeight)
+        {
+            var hwnd = WindowNative.GetWindowHandle(this);
+            var dpi = PInvoke.User32.GetDpiForWindow(hwnd);
+            var scalingFactor = dpi / 96.0;
+
+            var physicalWidth = (int)(logicalWidth * scalingFactor);
+            var physicalHeight = (int)(logicalHeight * scalingFactor);
+
+            AppWindow.Resize(new Windows.Graphics.SizeInt32(physicalWidth, physicalHeight));
         }
 
         private async void BtnSettings_Click(object sender, RoutedEventArgs e)
@@ -41,5 +57,15 @@ namespace ADO_Tools_WinUI
         {
             AppSettings.Default.Save();
         }
+
+        private static class PInvoke
+        {
+            public static class User32
+            {
+                [System.Runtime.InteropServices.DllImport("user32.dll")]
+                public static extern uint GetDpiForWindow(nint hwnd);
+            }
+        }
     }
 }
+
