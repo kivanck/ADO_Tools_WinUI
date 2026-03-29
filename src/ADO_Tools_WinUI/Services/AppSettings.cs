d86ee47f26e0fbe51d8ed36a24de77f33d3560bd
Kivanc Karakas <kivanckarakas@gmail.com>
2026-03-29 18:20:24 +0100

    packaged publishing updates

--- src/ADO_Tools_WinUI/Services/AppSettings.cs
+++ src/ADO_Tools_WinUI/Services/AppSettings.cs
@@ -15,19 +15,26 @@ namespace ADO_Tools_WinUI.Services
         private static AppSettings? _default;
         public static AppSettings Default => _default ??= Load();
 
-        public string RootFolder { get; set; } = @"D:\ADO_Items";
-        public string Organization { get; set; } = "";
-        public string Project { get; set; } = "";
-        public string WorkItemProject { get; set; } = "";
+        public string RootFolder { get; set; } = "";
+        public string Organization { get; set; } = "bentleycs";
+        public string Project { get; set; } = "civil";
+        public string WorkItemProject { get; set; } = "civil";
         public string SelectedQueryPath { get; set; } = "";
-        public string DefinitionId { get; set; } = "";
+        public string DefinitionId { get; set; } = "6098";
         public string PersonalAccessToken { get; set; } = "";
         public string DownloadFolder { get; set; } = "";
-        public List<string> UserDefinitionList { get; set; } = new();
+        public List<string> UserDefinitionList { get; set; } = new()
+        {
+            "OpenRail Designer|6098|civil",
+            "OpenRoads Designer|6057|civil",
+            "Overhead Line Designer|6289|civil",
+            "Microstation|5311|PowerPlatform",
+            "OpenBridge Designer|7391|civil"
+        };
         public int BuildCount { get; set; } = 30;
-        public string ProductName { get; set; } = "";
-        public string SearchAreaPath { get; set; } = "";
-        public string SearchCutoffDate { get; set; } = "2023-01-01";
+        public string ProductName { get; set; } = "OpenRail Designer";
+        public string SearchAreaPath { get; set; } = @"Civil\OpenCivil Designer";
+        public string SearchCutoffDate { get; set; } = "2020-01-01";
         public List<string> SearchResultColumns { get; set; } = new()
         {
             "System.Id", "System.Title", "System.State",
